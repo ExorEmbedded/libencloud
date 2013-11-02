0f80cdc964705bf3c5395a1ae5468e35450baf68
Steven Dorigotti <s.dorigotti@endian.com>
2013-11-02 10:18:39 +0100

    bindings: show sb_conf output in test

--- bindings/test.py
+++ bindings/test.py
@@ -4,10 +4,17 @@ print 'Runnings libece (v%s rev: %s) tests' % (ece.version(), ece.revision())
 
 e = ece.create(0, None)
 
+# SECE only
 ece.set_license(e, '{a8098c1a-f86e-11da-bd1a-00112444be1e}')
+
 ece.retr_sb_info(e)
 ece.retr_sb_cert(e)
-ece.retr_sb_conf(e)
+c = ece.retr_sb_conf(e)
+
+print "ip: " + ece.sb_conf_get_vpn_ip(c)
+print "port: " + str(ece.sb_conf_get_vpn_port(c))
+print "proto: " + ece.sb_conf_get_vpn_proto(c)
+print "type: " + ece.sb_conf_get_vpn_type(c)
 
 ece.destroy(e)
 
