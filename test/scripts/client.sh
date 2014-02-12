3a2deddc9921a8878c7b1532378c775f102f4c17
Steven Dorigotti <s.dorigotti@endian.com>
2014-02-12 15:34:32 +0100

    test/www: fix state not being updated (queue full)

--- test/scripts/client.sh
+++ test/scripts/client.sh
@@ -3,11 +3,22 @@
 [ $# -lt 1 ] && echo "Usage: $0 <action>" && exit 1
 
 HOST=localhost
-URL=/test
+URL=api_v1
 PORT=8081
 ACTION=$1
+ARGS=
 
-wget -O - ${HOST}:${PORT}/${URL} 2>/dev/null
+case ${ACTION} in
+    get)
+        URL=${URL}/status
+        ;;
+    setlic)
+        URL=${URL}/setup
+        ARGS="--post-data 'license=foobar'"
+        ;;
+esac
+
+wget -O - ${ARGS} ${HOST}:${PORT}/${URL} 2>/dev/null
 [ $? -ne 0 ] && echo "error contacting server!"
 
 exit 0
