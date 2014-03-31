dd3ce74bc380e10f4060783073496e25e4cdccaf
Steven Dorigotti <s.dorigotti@endian.com>
2014-03-31 11:12:38 +0200

    client: make compiler happy (no case values other than default)

--- src/common/client.cpp
+++ src/common/client.cpp
@@ -114,6 +114,7 @@ void Client::_networkError (QNetworkReply::NetworkError err)
 
     switch (reply->error())
     {
+        case -1:  // make compiler happy
         default:
             LIBENCLOUD_EMIT(error(Error(Error::CodeServerUnreach)));
             break;
