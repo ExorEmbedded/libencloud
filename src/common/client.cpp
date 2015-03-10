932ec80daab357ff3d2c3b5b1cfadaedf8b50835
Steven Dorigotti <s.dorigotti@endian.com>
2015-03-10 12:56:29 +0100

    client: allow self-signed certificates if validation is off

--- src/common/client.cpp
+++ src/common/client.cpp
@@ -129,6 +129,7 @@ void Client::_sslErrors (QNetworkReply *reply, const QList<QSslError> &errors)
     {
         switch (err.error())
         {
+            case QSslError::SelfSignedCertificate:
             case QSslError::SelfSignedCertificateInChain:
             case QSslError::UnableToGetLocalIssuerCertificate:
             case QSslError::UnableToVerifyFirstCertificate:
