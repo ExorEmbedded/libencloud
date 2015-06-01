05f4c90179a62ddd79511e1a10c9044d52a2c0a9
Steven Dorigotti <s.dorigotti@endian.com>
2015-06-01 16:31:58 +0200

    [FEATURE] CONNECT-209: Connection Profiles (Switchboard and plain VPN)
    Apply iface argument only to split nets

--- scripts/macx/route-up.sh
+++ scripts/macx/route-up.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-#
-# Avoids problems with split networks using tap device on OSX
+#!/bin/bash -e
 
 echo "Running custom route-up script"
 
@@ -10,5 +8,17 @@ for ((i = 1; i <= ${n_nets}; i++)); do
     network=$(eval echo \${route_network_${i}})
     gateway=$(eval echo \${route_gateway_${i}})
     netmask=$(eval echo \${route_netmask_${i}})
-    /sbin/route add -net ${network} -iface ${dev} ${gateway} ${netmask}
+
+    ARGS="add";
+    ARGS="${ARGS} -net ${network}"
+    ARGS="${ARGS} ${gateway}"
+    ARGS="${ARGS} ${netmask}"
+
+    # Avoids problems with split networks using tap device on OSX
+    # (possibly related: https://discussions.apple.com/message/8546386)
+    [ "${netmask}" = "255.255.255.128" ] && ARGS="${ARGS} -iface"
+
+    /sbin/route ${ARGS}
 done
+
+exit 0
