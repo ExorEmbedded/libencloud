1a442f5d78ea24b126b306b97d4fc8bfbabc5999
Steven Dorigotti <s.dorigotti@endian.com>
2015-09-21 16:38:00 +0200

    [FEATURE] CONNECT-268 Cookie-based authentication support

--- src/common/crypto.cpp
+++ src/common/crypto.cpp
@@ -68,6 +68,11 @@ int configFromAuth (const Auth &auth, QUrl &url, QMap<QByteArray,
             sslconf.setPrivateKey(key);
             break;
         }
+        case Auth::CookieType:
+        {
+            // do nothing - we assume cookie headers are set automatically
+            break;
+        }
         default:
             goto err;
     }
